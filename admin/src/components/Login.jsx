99b089573395b83337f34b82927fea798a60f918
Gull Bahisht <242188@students.au.edu.pk>
2026-09-05 00:51:15 +0500

    Fix admin login state

--- admin/src/components/Login.jsx
+++ admin/src/components/Login.jsx
@@ -4,8 +4,7 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 const Login = ({ setToken }) => {
-  const [email, setEmail] = useS
-  tate("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const onSubmitHandler = async (e) => {
